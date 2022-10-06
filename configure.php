40250d0541f6f0e9b3c0b2b9e168571938a09676
Akansha Rai <49139386+AkanR@users.noreply.github.com>
2022-10-06 20:01:38 +0530

    Update configure.php

--- configure.php
+++ configure.php
@@ -1,14 +1,14 @@
 <?php
 ob_start();
 session_start();
-$conn  = mysqli_connect("localhost","root","","tastebud");
+$conn  = mysqli_connect("localhost","root","","");
 require_once 'razorpay-php/Razorpay.php';
 require_once 'helperfunction.php';
 require_once 'adminhelperfunctions.php';
 require_once 'common.php';
 $merchant_name='TasteBud';
 $merchant_logo='images/finallogo.PNG';
-$keyId = 'rzp_test_8n34KuhXiDbwPi';
-$keySecret = 'YNfXDh4G301hEkuTw3QDwOAR';
+$keyId = '';
+$keySecret = '';
 $displayCurrency = 'INR';
 ?>
